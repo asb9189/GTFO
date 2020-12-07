acccfa62bb75629d68c2c042b41bfce87f972e71
asb9189 <asb9189@rit.edu>
2020-12-07 00:21:28 -0500

    Blood Animations

--- Scripts/BulletMovement.gd
+++ Scripts/BulletMovement.gd
@@ -4,18 +4,23 @@ var speed = 50
 var movement = Vector2()
 var mouse_pos
 var dir
+var room_width
+var room_height
 
 func init(dir):
 	self.dir = dir
+	
+func _ready():
+	room_width = get_viewport().size.x
+	room_height = get_viewport().size.y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if (position.x < 0 or position.x > room_width or position.y < 0 or position.y > room_height):
+		self_destroy()
 	movement = movement.move_toward(dir, delta)
 	movement = movement.normalized() * speed
 	position += movement
 	
-
-func _on_Bullet_body_entered(body):
-	if (body.name != "Player"):
-		body.health -= 1;
-		queue_free()
+func self_destroy():
+	queue_free()
--- Scripts/ZombieMovement.gd
+++ Scripts/ZombieMovement.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 var speed = 250
 var health = 3;
 var player_ref
+var animation_playing = false
+onready var animation_player = $"AnimatedSprite"
 
 func _ready():
 	player_ref = get_node("/root/Scene/Player")
@@ -16,4 +18,24 @@ func _process(delta):
 	var dir = (player_ref.position - self.position).normalized() * speed
 	rotation = (player_ref.position - position).angle()
 	move_and_slide(dir)
+	
+
+func _on_Area2D_area_entered(area):
+	if (area.name == "Bullet"):
+		health -= 1;
+		
+		if (not animation_playing):
+			animation_playing = true
+			animation_player.visible = true
+			animation_player.play("blood_splash_1")
+			
+		area.self_destroy()
+		
+
+
+func _on_AnimatedSprite_animation_finished():
+	animation_player.visible = false
+	animation_player.stop()
+	animation_player.set_frame(0)
+	animation_playing = false
 	
